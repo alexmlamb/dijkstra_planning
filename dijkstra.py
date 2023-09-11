d55fa92928e45ba4639d7b5b33822f08fabbd3cf
Your Name <you@example.com>
2023-09-11 11:21:05 -0700

    Updates

--- dijkstra.py
+++ dijkstra.py
@@ -1,35 +1,65 @@
 
 import time
 import sys
+import heapq
 
 def dijkstra(graph, start_node):
     # we keep visitation count, thus convert the count to (1-frequency)
     start = time.time()
-    unvisited_nodes = list(graph.keys())
+    nodes = list(graph.keys())
+    visited = {}
 
     # We'll use this dict to save the cost of visiting each node and update it as we move along the graph
     shortest_path = {}
+
+    shortest_path_heap = []
 
     # We'll use this dict to save the shortest known path to a node found so far
     previous_nodes = {}
 
     # We'll use max_value to initialize the "infinity" value of the unvisited nodes
     max_value = sys.maxsize
-    for node in unvisited_nodes:
+    for node in nodes:
         shortest_path[node] = max_value
+
+        heapq.heappush(shortest_path_heap, (max_value, node))
+
     # However, we initialize the starting node's value with 0
+    #shortest_path_heap.remove((max_value, start_node))
     shortest_path[start_node] = 0
+    heapq.heappush(shortest_path_heap, (0, start_node))
+
+    t_min = 0
+    t_neighb = 0
 
     algo_steps = 0
     # The algorithm executes until we visit all nodes
-    while unvisited_nodes:
+    while len(shortest_path_heap) > 0:
         # The code block below finds the node with the lowest score
-        current_min_node = None
-        for node in unvisited_nodes:  # Iterate over the nodes
-            if current_min_node == None:
-                current_min_node = node
-            elif shortest_path[node] < shortest_path[current_min_node]:
-                current_min_node = node
+        t0 = time.time()
+
+        cm_tuple = heapq.heappop(shortest_path_heap)
+        current_min_node = cm_tuple[1]
+
+        if current_min_node in visited:
+            continue
+        visited[current_min_node] = True
+
+        #set current_min_node to min value of shortest_path.  
+
+        #for node in unvisited_nodes:  # Iterate over the nodes
+        #    if current_min_node == None:
+        #        current_min_node = node
+        #    elif shortest_path[node] < shortest_path[current_min_node]:
+        #        current_min_node = node
+
+
+        t_min += time.time() - t0
+
+        t0 = time.time()
+
+        if not current_min_node in graph:
+            continue
 
         # The code block below retrieves the current node's neighbors and updates their distances
         neighbors = graph[current_min_node]
@@ -38,14 +68,27 @@ def dijkstra(graph, start_node):
             #print('neighbors', neighbors)
             #print('current min node', current_min_node)
             tentative_value = shortest_path[current_min_node] + 1#graph[current_min_node][neighbor]
-            if neighbor in shortest_path and tentative_value < shortest_path[neighbor]:
-                shortest_path[neighbor] = tentative_value
-                # We also update the best path to the current node
-                previous_nodes[neighbor] = current_min_node
+            if neighbor in shortest_path:
+                if tentative_value < shortest_path[neighbor]:
+
+                    shortest_path[neighbor] = tentative_value
+                    # We also update the best path to the current node
+                    previous_nodes[neighbor] = current_min_node
+
+                    heapq.heappush(shortest_path_heap, (tentative_value, neighbor))
 
         # After visiting its neighbors, we mark the node as "visited"
-        unvisited_nodes.remove(current_min_node)
+        #print('current_min_node', current_min_node)
+        #print(len(unvisited_nodes), len(shortest_path_heap))
+        #unvisited_nodes.remove(current_min_node)
+
+        t_neighb += time.time() - t0
+
     end = time.time() - start
+    #print('time to find min', t_min)
+    #print('time to process neighbors', t_neighb)
+    #print('end time', end)
+    #raise Exception()
     return previous_nodes, shortest_path, [algo_steps, end]
 
 
--- plan.py
+++ plan.py
@@ -20,16 +20,16 @@ def pick_goal(state_counts, state_dists):
 
         if not state in state_dists:
             score = 0.0
-        elif state_dists[state] == 0 or state_dists[state] > 10:
+        elif state_dists[state] == 0 or state_dists[state] > 100:
             score = 0.0
         else:
-            score = (1.0 / (1 + math.sqrt(state_counts[state]))) * (1.0 / max(1, state_dists[state]))
+            score = (1.0 / (1.0 + math.sqrt(state_counts[state]))) * (1.0 / max(1, state_dists[state]))
         scores.append(score)
  
     scores = numpy.array(scores)
 
     if scores.sum() < 1e-3:
-        print('planning failure - no goals found')
+        #print('planning failure - no goals found')
         return None, -1
 
     scores = scores / (scores.sum())
@@ -43,15 +43,35 @@ def pick_goal(state_counts, state_dists):
 
     return goal, steps_to_goal
 
-def pick_action(allowable_actions, action_counts): 
-    return random.choice(allowable_actions)
+def pick_action(action_counts, allowed_actions): 
+
+    scores = []
+
+    for action in allowed_actions: 
+        if not action in action_counts:
+            action_counts[action] = 0
+        score = 1.0 / (1.0 + math.sqrt(action_counts[action]))
+        scores.append(score)
+
+    scores = numpy.array(scores)
+
+    if scores.sum() < 1e-3:
+        return random.choice(allowed_actions)
+
+    scores = scores/scores.sum()
+
+    score_idx = numpy.random.choice(numpy.arange(len(allowed_actions)), p=scores)
+
+    action = allowed_actions[score_idx]
+
+    return action
 
 
 # Either picks a goal or picks an action randomly from set of allowable actions.  
 # If goal=None or steps_to_goal=0, act randomly and then set a new goal and steps_to_goal.  
 # Otherwise, follow to the goal and reduce steps_to_goal by 1.  
 
-def get_action(screenshot, page_navigation_actions, direct_ui_actions, state_dict, action_id):
+def get_action(screenshot, page_navigation_actions, direct_ui_actions, state_dict, action_id, config):
 
     allowed_actions = page_navigation_actions + direct_ui_actions
 
@@ -65,54 +85,74 @@ def get_action(screenshot, page_navigation_actions, direct_ui_actions, state_dic
     goal = state_dict['goal']
     steps_to_goal = state_dict['steps_to_goal']
 
+    if config['rare_action_policy']:
+        chosen_action = pick_action(action_counts, allowed_actions)
+        return chosen_action, state_dict
+
     opt_acts, opt_dists, _ = dijkstra(graph, start_state)
 
 
+    if opt_dists[config['home_state']] > 1000:
+        #print('cannot find route home!  Should trigger reset to VM?')
+        state_dict['no_way_home'] = True
+    else:
+        state_dict['no_way_home'] = False
 
     #Did we hit goal, not have goal, or run out of steps?  
     if goal is None or goal == start_state or steps_to_goal <= 0 or not (start_state in graph.keys()):
+
+        if goal == start_state:
+            state_dict['hit_goal'] += 1
+
         #random action and find a new goal.  
-        chosen_action = random.choice(allowed_actions)
+        chosen_action = pick_action(action_counts, allowed_actions)
 
         goal, steps_to_goal = pick_goal(state_counts, opt_dists)
+        steps_to_goal += config['extra_plan_steps']
 
         state_dict['goal'] = goal
         state_dict['steps_to_goal'] = steps_to_goal
 
+
     else:
-        print('try to hit goal', goal, 'in steps', steps_to_goal)
-        #return action for goal and decrement steps to goal.  
-        if goal in opt_acts and opt_dists[goal] <= steps_to_goal + 1:
+
+        if goal in opt_acts and opt_dists[goal] <= steps_to_goal:
             plan_seq = backtrack(opt_acts, start_state, goal)
-            print('planned sequence to goal', plan_seq)
+            #print('planned sequence to goal', plan_seq)
             chosen_action = random.choice(inv[(plan_seq[0], plan_seq[1])])
             state_dict['plan_acts'] += 1
-            print('action to execute', chosen_action)
+            #print('action to execute', chosen_action)
         else:
-            chosen_action = random.choice(allowed_actions)
+            
+            chosen_action = pick_action(action_counts, allowed_actions)
             state_dict['goal'] = None
             state_dict['steps_to_goal'] = -1
+            state_dict['plan_fail'] += 1
 
         state_dict['steps_to_goal'] -= 1
 
-
     return chosen_action, state_dict
 
-def simulate_env(graph, inv, state, action):
+def simulate_env(graph, inv, state, action, config):
     assert type(action) is str
-    default = '03e5f372b4c1fb6059b95ea61365de30'
+
+    default = config['home_state']
 
     if not state in graph:
         return default
+
+    next_lst = []
 
     next_states = graph[state]
     for ns in next_states:
         for inv_action in inv[(state, ns)]:
             if inv_action == action:
-                return ns
-    
-    #print('no successor found from', state, action, 'reset to start')
-    return default
+                next_lst.append(ns)
+
+    if len(next_lst) >= 1:
+        return random.choice(next_lst)
+    else:
+        return default
 
 def init_state_dict(graph, inv):
 
@@ -134,44 +174,59 @@ def init_state_dict(graph, inv):
     state_dict['steps_to_goal'] = -1
     state_dict['inv'] = inv
     state_dict['plan_acts'] = 0
+    state_dict['plan_fail'] = 0
+    state_dict['hit_goal'] = 0
+    state_dict['no_way_home'] = False
 
     return state_dict
 
 if __name__ == "__main__":
 
+    config = {}
+    config['extra_plan_steps'] = 1.0  #This is how many extra steps we give the model to reach a plan.  
+    config['p_reset_nowayhome'] = 0.1 #If we can't find a path to home state, then we hard-reset with this probability.  
+    config['home_state'] = '03e5f372b4c1fb6059b95ea61365de30'
+    config['rare_action_policy'] = False # Just act by sampling rarely-taken actions, with no planning
+
     graph, inv, allowed_actions = get_graph()
 
     state_dict = init_state_dict(graph, inv)
 
-    #Follow a data-collection loop and pretend that the graph is the true dynamics.  
-
-    start_state = '03e5f372b4c1fb6059b95ea61365de30'
+    start_state = config['home_state']
 
     print('start', start_state)
     print('re-encode', encode(allowed_actions[start_state]))
     assert start_state == encode(allowed_actions[start_state])
 
-    #Initially get it to take random action, then try to get it to reach a goal.  
-    #action, state_dict = get_action(screenshot=None, page_navigation_actions=allowed_actions[start_state], direct_ui_actions=[], state_dict=state_dict, action_id=None)
+    random_policy = False
 
-    random_policy = True
+    for j in range(0, 2000000):
 
-    for j in range(0, 20000000):
-        
         if random_policy:
             action = random.choice(allowed_actions[start_state])
+            no_route_home = False
         else:
-            action, state_dict = get_action(screenshot=None, page_navigation_actions=allowed_actions[start_state], direct_ui_actions=[], state_dict=state_dict, action_id=None)
+            action, state_dict = get_action(screenshot=None, page_navigation_actions=allowed_actions[start_state], direct_ui_actions=[], state_dict=state_dict, action_id=None, config=config)
 
-        print(j, start_state, action)
-        print('steps-to-goal', state_dict['steps_to_goal'])
-        start_state = simulate_env(graph, inv, start_state, action)
+        if j % 100 == 0:
+            print(j, start_state, action)
+            print('steps-to-goal', state_dict['steps_to_goal'])
+        start_state = simulate_env(graph, inv, start_state, action, config)
+
+        if state_dict['no_way_home'] and random.uniform(0,1) < config['p_reset_nowayhome']:
+            start_state = config['home_state']
 
         if not start_state in state_dict['state_counts']:
             state_dict['state_counts'][start_state] = 0
         state_dict['state_counts'][start_state] += 1
 
+        if not action in state_dict['action_counts']:
+            state_dict['action_counts'][action] = 0
+        state_dict['action_counts'][action] += 1
+
     print('number planned actions', state_dict['plan_acts'])
+    print('number of goal-hits', state_dict['hit_goal'])
+    print('number of planning-fails', state_dict['plan_fail'])
 
     num_visited = 0
     for state in state_dict['state_counts']:
@@ -179,27 +234,13 @@ if __name__ == "__main__":
             num_visited += 1
     print('number visited states', num_visited)
 
-if False:
+    num_actions = 0
+    for action in state_dict['action_counts']:
+        if state_dict['action_counts'][action] > 0:
+            num_actions += 1
 
-    graph, inv = get_graph()
+    print('num actions visited', num_actions)
 
-    #start = '03e5f372b4c1fb6059b95ea61365de30'
-
-    paths, dist, _ = dijkstra(graph, start)
-
-    print(dist)
-
-    #goal = '8f8b4ff07b75d46c05fe290433222130'
-    #goal = '03e5f372b4c1fb6059b95ea61365de30'
-    goal = 'e838873f7a0e6d0d8465cf31408fd9ef'
-
-    p = backtrack(paths, start, goal)
-
-    print('plan from', start, 'to', goal)
-
-    print(p[0])
-    for j in range(0, len(p)-1):
-        print(inv[(p[j], p[j+1])], p[j+1])
 
 
 
